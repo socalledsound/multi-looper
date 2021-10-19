1748e6d2490d2649348d6968ebea427549d3abca
Chris Kubick <socalledsound@gmail.com>
2021-10-19 00:22:14 -0700

    update pitch fader

--- CONTROLS/Controls.js
+++ CONTROLS/Controls.js
@@ -6,7 +6,7 @@ class Controls{
         this.h = h
         this.playLoopButton = new PlayButton(this.x + this.w * 0.6, this.y + this.h - this.h/2, this.w/7, this.h/6, 'loop')
         this.playOnceButton = new PlayButton(this.x + this.w * 0.4, this.y + this.h - this.h/2, this.w/7, this.h/6, 'once')
-        this.pitchControl = new PitchControl(this.x + this.w/3, this.y + this.h - this.h/7, this.w * 0.5, this.h/10)
+        this.pitchControl = new PitchControl(this.x + this.w/3, this.y + this.h - this.h/7, this.w * 0.5, this.h/6)
         this.volumeControl = new VolumeControl(this.x + this.w/6, this.y + this.h/6, this.w/10, this.h * 0.9)
         // this.controls = [this.playLoopButton, this.playOnceButton, this.pitchControl, this.VolumeControl]
         this.controls = [this.playLoopButton, this.playOnceButton, this.pitchControl,this.volumeControl]
--- CONTROLS/SliderHorizontal.js
+++ CONTROLS/SliderHorizontal.js
@@ -8,13 +8,15 @@ class SliderHorizontal{
         this.defaultColor = [200,200,200,60]
         this.hoverColor = [200,200,200,120]
         this.activeColor = [200,200,200,200]
+        this.faderLineColor = [20,20,120,220]
         this.color = this.defaultColor
-        this.knob = new Knob(this.x + this.w/4, this.y, this.w/20, this.h, this.x, this.w)
-        this.marks = Array.from({length: 60}, (el, i) => {
+        this.knob = new Knob(this.x + this.w/2, this.y - 5, this.w/20, this.h + 10, this.x, this.w)
+        this.numMarks = 60
+        this.marks = Array.from({length: this.numMarks}, (el, i) => {
             return {
-                x1: this.x + i * 5,
+                x1: this.x + i * this.w/this.numMarks,
                 y1: this.y,
-                x2: this.x + i * 5,
+                x2: this.x + i * this.w/this.numMarks,
                 y2: this.y + this.h
             }
         })
@@ -37,6 +39,9 @@ class SliderHorizontal{
             strokeWeight(1)
             line(mark.x1, mark.y1, mark.x2, mark.y2)
         })   
+        stroke(this.faderLineColor)
+        strokeWeight(1)
+        line(this.x - 5, this.y + this.h/2, this.x + this.w, this.y + this.h/2)
         this.knob.render() 
     }
 }
